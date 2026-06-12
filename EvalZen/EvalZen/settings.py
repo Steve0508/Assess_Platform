6562a687bdbf0cf1da4cca86e1de717da4b46f4f
Praveen <praveenkal0508@gmail.com>
2026-06-12 19:40:17 +0530

    Fix production config

--- EvalZen/EvalZen/settings.py
+++ EvalZen/EvalZen/settings.py
@@ -130,6 +130,8 @@ ROOT_URLCONF = 'EvalZen.urls'
 # TEMPLATES
 TEMPLATES = [
     {
+
+        
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
